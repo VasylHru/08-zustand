214573641cf6f7952396d0bd8f089d95f5c91e8a
Vasyl Hrubyi <ronaldomen7@gmail.com>
2025-12-15 14:07:45 +0200

    add-botonn-in-note-page

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,9 +1,13 @@
 "use client";
-
+import { useRouter } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 
+
+
+
 export default function NoteDetailsClient({ id }: { id: string }) {
+  const router = useRouter();
   const { data, isLoading, isError } = useQuery({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
@@ -13,8 +17,16 @@ export default function NoteDetailsClient({ id }: { id: string }) {
   if (isLoading) return <p>Loading, please wait...</p>;
   if (isError || !data) return <p>Error loading note</p>;
 
+  
+  const handleGoBack = () => {
+    const isSure = confirm("Are you sure?");
+    if (isSure) {
+      router.back();
+    }
+  };
   return (
     <>
+      <button onClick={handleGoBack}>Back</button>
       <h2>{data.title}</h2>
       <p>{data.content}</p>
       <p>{data.createdAt}</p>
